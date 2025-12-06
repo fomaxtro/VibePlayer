1860d951c80f24552e50f879e514934f2e0d9e05
Felipe Oyarzun <oyarzunfelipe6@gmail.com>
2025-12-06 19:35:34 -0300

    feat(library): Add RadioGroup and OutlinedRadioButton components
    Introduces new UI components for creating radio button groups within the `feature.library` module.
    
    **`OutlinedRadioButton.kt`**
    *   Adds a new `OutlinedRadioButton` composable.
    *   It consists of a `RadioButton` and a `Text` label wrapped in a `Row` with a circular border.
    *   The border color dynamically changes based on the `selected` state.
    
    **`RadioGroup.kt`**
    *   Adds a `RadioGroup` composable to arrange radio buttons.
    *   It displays a title `Text` and uses a `FlowRow` to lay out its content, ensuring responsiveness by wrapping items into the next row if space is insufficient.

---
+++ feature/library/src/main/java/com/fomaxtro/vibeplayer/feature/library/component/OutlinedRadioButton.kt
@@ -0,0 +1,75 @@
+package com.fomaxtro.vibeplayer.feature.library.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fomaxtro.vibeplayer.core.designsystem.theme.VibePlayerTheme
+import com.fomaxtro.vibeplayer.core.designsystem.theme.buttonPrimary30
+import com.fomaxtro.vibeplayer.core.designsystem.theme.surfaceOutline
+
+@Composable
+fun OutlinedRadioButton(
+    selected: Boolean,
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+    shape: Shape = CircleShape
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = if (selected) {
+                    MaterialTheme.colorScheme.buttonPrimary30
+                } else {
+                    MaterialTheme.colorScheme.surfaceOutline
+                },
+                shape = shape
+            )
+            .clip(shape)
+            .clickable(onClick = onClick)
+            .padding(8.dp)
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick = null,
+            modifier = Modifier.size(28.dp)
+        )
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun OutlinedRadioButtonPreview() {
+    VibePlayerTheme {
+        OutlinedRadioButton(
+            onClick = {},
+            selected = false,
+            text = "30s",
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
